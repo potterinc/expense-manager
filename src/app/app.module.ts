bd7e7d49f7476ca6108f19ec81b1e9d37f5eb671
Arekhandia Harry <harry_p30@yahoo.com>
2022-04-04 18:25:23 +0100

    Added FormsModule and ReactiveForms Module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { DatabindingComponent } from './components/test/databinding/databinding.component';
+import { AddExpenseComponent } from './components/add-expense/add-expense.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     DatabindingComponent,
+    AddExpenseComponent,
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]

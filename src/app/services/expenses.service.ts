471ef86a30840bdf8b8ac62f742d8c8bc656473d
Arekhandia Harry <harry_p30@yahoo.com>
2022-04-06 00:05:40 +0100

    completed addExpenseRecord service algorithm.

--- src/app/services/expenses.service.ts
+++ src/app/services/expenses.service.ts
@@ -1,9 +1,19 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Expense } from '../models/expense';
+import { ApiResponse } from '../models/api-response';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class ExpensesService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+  baseUrl = "http://localhost/ngCRUD/";
+
+  addExpenseRecord(expenses: Expense): Observable<ApiResponse> {
+    return this.http.post<ApiResponse>(this.baseUrl + 'insert.php', expenses)
+  }
 }
